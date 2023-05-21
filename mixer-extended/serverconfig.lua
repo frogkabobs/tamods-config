0fd4f12237548b6c6e7066e5d62efb2b91712682
frogkabobs <frogkabobs@gmail.com>
2023-05-20 21:19:26 -0500

    fixed direct

--- mixer-extended/serverconfig.lua
+++ mixer-extended/serverconfig.lua
@@ -262,6 +262,7 @@ VehicleWeapons.setProperty("Beowulf Chaingun", VehicleWeapons.Properties.ClipAmm
 VehicleWeapons.setProperty("Beowulf Chaingun", VehicleWeapons.Properties.FireInterval, 0.2)
 VehicleWeapons.setProperty("Beowulf Chaingun", VehicleWeapons.Properties.ReloadTime, 1.8)
 VehicleWeapons.setProperty("Beowulf Chaingun", VehicleWeapons.Properties.DeviceProjectile, Projectiles.getId("dustdevil"))
+
 Projectiles.setProperty("dustdevil", Projectiles.Properties.MaxDamageRangeProportion, 0.0)
 Projectiles.setProperty("dustdevil", Projectiles.Properties.MinDamageRangeProportion, 1.0)
 Projectiles.setProperty("dustdevil", Projectiles.Properties.MinDamageProportion, 0.5)
@@ -398,6 +399,16 @@ Items.setProperty("Heavy", "apxl", Items.Properties.ExplosiveRadius, 800.00)
 Items.setProperty("Heavy", "apxl", Items.Properties.ExplodeOnContact, true)
 Items.setProperty("Heavy", "apxl", Items.Properties.MustBounceBeforeExplode, false)
 ]]
+
+Items.setProperty("light", "duelingspin", Items.Properties.DirectHitMultiplier, 1.25)
+Items.setProperty("light", "stealthspin", Items.Properties.DirectHitMultiplier, 1.25)
+Items.setProperty("heavy", "spinfusormkd", Items.Properties.DirectHitMultiplier, 1.25)
+Items.setProperty("medium", "dustdevil", Items.Properties.MaxDamageRangeProportion, 0.0)
+Items.setProperty("medium", Items.Properties.MinDamageRangeProportion, 1.0)
+Items.setProperty("medium", Items.Properties.MinDamageProportion, 0.5)
+Items.setProperty("medium", Items.Properties.Damage, 300.0)
+Items.setProperty("medium", Items.Properties.DirectHitMultiplier, 1.25)
+
 require("admin")
 
 function dump(o)
